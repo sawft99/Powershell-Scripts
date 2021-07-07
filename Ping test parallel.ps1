e86a3bdc6e024b0b5f9c04ae1820dec974a43f81
sawft99 <81699231+sawft99@users.noreply.github.com>
2021-07-07 11:47:59 -0400

    More reporting and ping change
    Changed to test-connection form ping per #3 and added some better reporting.

--- Ping test parallel.ps1
+++ Ping test parallel.ps1
@@ -26,7 +26,7 @@ Options:
 do {
     $ShortOrLong = Read-Host -Prompt "Select an option"
     if ($ShortOrLong -notin 1..2) {
-        Write-Warning "Please pick a valid option"
+        Write-Warning "Please pick a valid option."
     }
 }
 while ($ShortOrLong -notin 1..2)
@@ -35,26 +35,32 @@ Write-Host ""
 #Parallel ping. Results recorded to each appropriate file
 if ($ShortOrLong -eq 1) {
     Write-Host "Running short ping test..."
+    Write-Host ""
     $Custom2 | ForEach-Object -ThrottleLimit 20 -Verbose -Parallel {
         $AliveTest = Test-Connection -Count 3 -Ping -IPv4 -DontFragment -TargetName $_
-        if ($AliveTest.status -notcontains "Success") {
-            Write-Warning "Dead host at $_ or high latency. Skipping"
+        $AliveTest | Out-File -FilePath $using:OutLocation\$_.txt
+        if ($AliveTest.Status -notcontains "Success") {
+            Write-Warning "Dead host at $_ or high latency. Skipping."
+            Write-Output "Dead host at $_ or high latency. Skipping." | Out-File -FilePath $using:OutLocation\$_.txt -Force -Append
         }
         else {
-            ping -n 50 $_ | Out-File -FilePath $using:OutLocation\$_.txt -Force -Append    
+            Test-Connection -Count 50 -Ping -IPv4 -DontFragment -TargetName $_ | Out-File -FilePath $using:OutLocation\$_.txt -Force -Append
         }
     }
 }
 
 if ($ShortOrLong -eq 2) {
     Write-Host "Running long ping test..."
+    Write-Host ""
     $Custom2 | ForEach-Object -ThrottleLimit 20 -Verbose -Parallel {
         $AliveTest = Test-Connection -Count 3 -Ping -IPv4 -DontFragment -TargetName $_
+        $AliveTest | Out-File -FilePath $using:OutLocation\$_.txt
         if ($AliveTest.status -notcontains "Success") {
-            Write-Warning "Dead host at $_ or high latency. Skipping"
+            Write-Warning "Dead host at $_ or high latency. Skipping."
+            Write-Output "Dead host at $_ or high latency. Skipping." | Out-File -FilePath $using:OutLocation\$_.txt -Force -Append
         }
         else {
-            ping -n 250 $_ | Out-File -FilePath $using:OutLocation\$_.txt -Force -Append
+            Test-Connection -Count 250 -Ping -IPv4 -DontFragment -TargetName $_ | Out-File -FilePath $using:OutLocation\$_.txt -Force -Append
         }
     }
 }
@@ -63,22 +69,38 @@ if ($ShortOrLong -eq 2) {
 $Files = Get-ChildItem $OutLocation
 
 #Write result to host/console (Reports issues only)
+Write-Host ""
+Write-Host "Final results:"
+Write-Host ""
 foreach ($File in $Files) {
     $Content = Get-Content -Path $File.FullName
     $Filename = $File.BaseName
-    if ($Content -match 'timed out') {
-        Write-host "$Filename dropped packets"
+    if ($Content -match 'TimedOut') {
+        if ($Content -match 'Dead') {
+            Write-Warning "Dead host at $Filename or high latency."
+        }
+        else {
+            Write-Host "Dropped packets at $Filename."
+        }
     }
 }
+Write-Host ""
 
 #Write results to file (All results recorded)
 foreach ($File in $Files) { 
     $Content = Get-Content -Path $File.FullName
     $Filename = $File.BaseName
-    if ($Content -match 'timed out') {
-        Write-Output "$Filename dropped packets" | Out-file -Force -Path $Results -Append
+    if ($Content -match 'TimedOut') {
+        if ($Content -match 'Dead') {
+            Write-Output "$Filename did not respond." | Out-file -Path $Results -Force -Append
+        }
+        else {
+            Write-Output "$Filename dropped packets." | Out-file -Path $Results -Force -Append
+        }
     }
     else {
-        Write-Output "$Filename is ok" | Out-File -Force -Path $Results -Append
+        Write-Output "$Filename is ok. Check for latency." | Out-File -Path $Results -Force -Append
     }
 }
+Write-Host "All results recorded to $Outocation\Results.txt."
+Write-Host ""

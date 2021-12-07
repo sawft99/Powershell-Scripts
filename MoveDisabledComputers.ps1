0a9062f1840bdbb490d50ebdc3a6996949a84f43
sawft99 <81699231+sawft99@users.noreply.github.com>
2021-12-06 22:03:51 -0500

    Variable and logic updates
    Updates to variable and logic. Variable names more generic and greater than comparison no longer locked into a specific number. Will match variable set at start now.

--- MoveDisabledComputers.ps1
+++ MoveDisabledComputers.ps1
@@ -2,26 +2,26 @@
 #DisableDateCutoff and DeleteDateCutoff dates measured in days. Example '$DisableDateCutoff = 90' for 90 days less than current date
 
 $Base = #"OU=Computers,DC=Domain,DC=Local"
-$NewOU = #"OU=DisabledComputers,DC=Domain,DC=Local"
+$DisabledPCsOU = #"OU=DisabledComputers,DC=Domain,DC=Local"
 $DisableDateCutoff = 90
 $DeleteDateCutoff = 120
 $CurrentDate = Get-Date
-$90DaysAgo = $CurrentDate.AddDays(-$DisableDateCutoff)
-$120DaysAgo = $CurrentDate.AddDays(-$DeleteDateCutoff)
+$DisableDate = $CurrentDate.AddDays(-$DisableDateCutoff)
+$DeleteDate = $CurrentDate.AddDays(-$DeleteDateCutoff)
 
 $Computers = Get-ADComputer -SearchBase $Base -SearchScope 2 -filter *
 
 ForEach ($Computer in $Computers) {
-    If ((New-TimeSpan -Start $90DaysAgo -End $CurrentDate).Days -GT 90) {
+    If ((New-TimeSpan -Start $DisableDate -End $CurrentDate).Days -GT $DisableDateCutoff) {
         Set-ADComputer -Identity $Computer.ObjectGUID -Enabled $false
-        Move-ADObject -Identity $Computer.ObjectGUID -TargetPath $NewOU 
+        Move-ADObject -Identity $Computer.ObjectGUID -TargetPath $DisabledPCsOU 
     }
 }
 
-$DisabledComputers = Get-ADComputer -SearchBase $NewOU -SearchScope 2 -filter *
+$DisabledComputers = Get-ADComputer -SearchBase $DisabledPCsOU -SearchScope 2 -filter *
 
 ForEach ($DisabledComputer in $DisabledComputers) {
-    If ((New-TimeSpan -Start $120DaysAgo -End $CurrentDate).Days -GT 120) {
+    If ((New-TimeSpan -Start $DeleteDate -End $CurrentDate).Days -GT $DeleteDateCutoff) {
         Remove-ADComputer -Identity $DisabledComputer.ObjectGUID
     }
 }

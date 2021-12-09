24593ea1606734a0783930a6b5a9a187329b2e1e
sawft99 <81699231+sawft99@users.noreply.github.com>
2021-12-09 15:04:35 -0500

    Add comments
    Add comments

--- MoveDisabledComputers.ps1
+++ MoveDisabledComputers.ps1
@@ -1,16 +1,21 @@
 #Script that finds disabled computers and moves them to an OU.
 #DisableDateCutoff and DeleteDateCutoff dates measured in days. Example '$DisableDateCutoff = 90' for 90 days less than current date
 
+#Working OUs and date cutoff variables
 $Base = #"OU=Computers,DC=Domain,DC=Local"
 $DisabledPCsOU = #"OU=DisabledComputers,DC=Domain,DC=Local"
 $DisableDateCutoff = 90
 $DeleteDateCutoff = 120
+
+#Create dates for disable and delete thresholds
 $CurrentDate = Get-Date
 $DisableDate = $CurrentDate.AddDays(-$DisableDateCutoff)
 $DeleteDate = $CurrentDate.AddDays(-$DeleteDateCutoff)
 
+#Get all computers in OU and subtree.
 $Computers = Get-ADComputer -SearchBase $Base -SearchScope 2 -filter *
 
+#If computer has not been logged into for X days move it
 ForEach ($Computer in $Computers) {
     If ((New-TimeSpan -Start $DisableDate -End $CurrentDate).Days -GT $DisableDateCutoff) {
         Set-ADComputer -Identity $Computer.ObjectGUID -Enabled $false
@@ -18,8 +23,10 @@ ForEach ($Computer in $Computers) {
     }
 }
 
+#Find disabled computers in the $DisabledPCsOU
 $DisabledComputers = Get-ADComputer -SearchBase $DisabledPCsOU -SearchScope 2 -filter *
 
+#Delete PCs in OU that have not been logged into for X days
 ForEach ($DisabledComputer in $DisabledComputers) {
     If ((New-TimeSpan -Start $DeleteDate -End $CurrentDate).Days -GT $DeleteDateCutoff) {
         Remove-ADComputer -Identity $DisabledComputer.ObjectGUID

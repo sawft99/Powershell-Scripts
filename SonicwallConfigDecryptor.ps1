6a6226858eddca7b0fc7c405af4a22bf421cb0e6
sawft99 <81699231+sawft99@users.noreply.github.com>
2021-12-10 12:47:12 -0500

    Added notes
    Added notes

--- SonicwallConfigDecryptor.ps1
+++ SonicwallConfigDecryptor.ps1
@@ -1,22 +1,30 @@
 #Decrypt Sonicwall .exp settings file
 #Made for Windows
 
+#Location of .EXP file to decrypt
 $OriginalSettingsFile = "C:\WorkingDirectory\settings.exp"
+
+#Premade variables will do all of the work in the same directory
 $OriginalSettings = Get-Content $OriginalSettingsFile
 $ParentDirectory = $OriginalSettings.PSParentPath
 $TempSettingsFile = "$ParentDirectory\TempSettings.xps"
 $DecodedSettingsFile = "$ParentDirectory\TempSettings.txt"
 $FinalFile = "$ParentDirectory\DecryptedSettings.txt"
 
+#Trim charecters at end so it can be properly decoded
 $OriginalSettingsTrim1 = $OriginalSettings.TrimEnd("&&")
 $OriginalSettingsTrim1 | Out-File -FilePath $TempSettingsFile
 
+#Decode file
 certutil.exe -decode $TempSettingsFile $DecodedSettingsFile
 
+#Change charecters in file
 $DecodedContent = Get-Content $DecodedSettingsFile
 $FinalContent = $DecodedContent.Replace("&", "`n")
 
+#Final decrypted file
 $FinalContent | Out-File $FinalFile
 
+#Cleanup
 Remove-Item $TempSettingsFile
 Remove-Item $DecodedSettingsFile

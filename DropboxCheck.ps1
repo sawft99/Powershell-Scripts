63248fde3965789ab3fdd296e27229add7869e0b
sawft99 <81699231+sawft99@users.noreply.github.com>
2022-08-12 10:47:13 -0400

    Format changes

--- DropboxCheck.ps1
+++ DropboxCheck.ps1
@@ -1,21 +1,21 @@
-#Checks program files and appdata for dropbox client. Designed for an external app to react to the info the script reocrds.
+#Checks program files and appdata for dropbox client.
 
 $OutLocation = #Location
-$usersfolder = get-childitem C:\users
+$usersfolder = get-childitem C:\Users
 
 foreach ($user in $usersfolder) {
     if (Test-Path C:\Users\$user\appdata\roaming\Dropbox\bin\Dropbox.exe) {
         $results = "Exists"
         $results | Out-File $OutLocation\dropboxdetect.txt
-        }
     }
+}
 
 if (Test-Path "C:\Program Files (x86)\Dropbox\Client\Dropbox.exe") {
     $results = "Exists"
     $results | Out-File $OutLocation\dropboxdetect.txt
-    }
+}
     
 if ($null -eq $results) {
     $results = "None"
     $results | Out-File $OutLocation\dropboxdetect.txt
-    }
+}

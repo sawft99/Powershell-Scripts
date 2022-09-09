a7b1a26d387870b0eb52d52a4e8315df399040a2
sawft99 <81699231+sawft99@users.noreply.github.com>
2022-09-09 15:57:44 -0400

    Format and logic changes

--- RealtekSoundReset.ps1
+++ RealtekSoundReset.ps1
@@ -4,54 +4,58 @@ $Error.clear()
 
 #Check if already elevated. Will relaunch elevated if not.
 $ElevCheck = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
-
-if ($ElevCheck -eq $fasle) {
-    Start-Process $PSCommandPath -Verb runas
-    timeout.exe /nobreak /t 3
-} 
+if ($ElevCheck -eq $false) {
+    Start-Process powershell.exe -Verb runas -ArgumentList "-File", "$PSCommandPath"
+    timeout.exe /t 3
+    Exit
+}
 else {
-    Start-Process -FilePath Powershell -Verb runas -ArgumentList {
-        #Check for Realtek sound hardware
-        $HardwareCheck = (Get-PnpDevice -FriendlyName 'Realtek(R) Audio' -ErrorAction SilentlyContinue).InstanceID
-        if ($Null -eq $HardwareCheck -or $HardwareCheck.Count -eq 0) {
-            Write-Host -ForegroundColor Red "Could not find Realtek audio device..."
-            Write-Host ""
-            Pause
-            Exit
-        }
-        $Error.Clear()
-        #Disable audio device
-        Write-Host "Disabling adapter..."
-        Disable-PnpDevice "$HardwareCheck" -Confirm:$false -ErrorAction SilentlyContinue
-        if ($Error.Count -gt 0) {
-            Write-Host ""
-            Write-Host -ForegroundColor Red "Issue disabling. Check that nothing besides the system is using sound and then rerun."
-            SndVol.exe
-            Write-Host ""
-            Pause
-            Exit
-        } 
-        else {
-            #Wait several seconds to allow operation
-            timeout.exe /nobreak /t 3
-            $Error.Clear()
-            Write-Host ""
-            Write-Host "Disabled successfully!"
-            Write-Host ""
-            #Enable audio device
-            Write-Host "Enabling adapter..."
-            Enable-PnpDevice "$HardwareCheck" -Confirm:$false
-            Write-Host ""
-            if ($Error.Count -gt 0) {
-                Write-Host -ForegroundColor Red "Issue enabling"
-                Write-Host ""
-                Pause
-                Exit
-            } else {
-                Write-Host "Enabled successfully!"
-            }
-        timeout /t 3
+    #Check for Realtek sound hardware
+    $HardwareCheck = (Get-PnpDevice -FriendlyName 'Realtek(R) Audio' -ErrorAction SilentlyContinue)
+    $HardwareID = $HardwareCheck.InstanceId
+    if ($HardwareCheck.Problem -eq "CM_PROB_DISABLED") {
+        Write-Host -ForegroundColor Yellow "Realtek audio device already disabled. Exiting..."
+        timeout.exe /t 5
         Exit
+    }
+    if ($Null -eq $HardwareID.Count -or $HardwareID.Count -eq 0) {
+        Write-Host -ForegroundColor Red "Could not find Realtek audio device..."
+        Write-Host ""
+        Pause
+        Exit
+    }
+    $Error.Clear()
+    #Disable audio device
+    Write-Host "Disabling adapter..."
+    Disable-PnpDevice "$HardwareID" -Confirm:$false -ErrorAction SilentlyContinue
+    if ($Error.Count -gt 0) {
+        Write-Host ""
+        Write-Host -ForegroundColor Red "Issue disabling. Check that nothing besides the system is using sound and then rerun."
+        SndVol.exe
+        Write-Host ""
+        Pause
+        Exit
+    } 
+    else {
+        #Wait several seconds to allow operation
+        $Error.Clear()
+        Write-Host ""
+        Write-Host -ForegroundColor Green "Disabled successfully!"
+        timeout.exe /nobreak /t 3
+        Write-Host ""
+        #Enable audio device
+        Write-Host "Enabling adapter..."
+        Enable-PnpDevice "$HardwareID" -Confirm:$false
+        Write-Host ""
+        if ($Error.Count -gt 0) {
+            Write-Host -ForegroundColor Red "Issue enabling"
+            Write-Host ""
+            Pause
+            Exit
+        } else {
+            Write-Host -ForegroundColor Green "Enabled successfully!"
         }
+    timeout /t 5
+    Exit
     }
 }

81fb27952c21dbba471aba9efce8848403e7a94d
sawft99 <81699231+sawft99@users.noreply.github.com>
2023-08-04 21:17:14 -0500

    Create CollatzConjecture.ps1

---
+++ CollatzConjecture.ps1
@@ -0,0 +1,29 @@
+#A demo of the Collatz Conjecture Theory
+#If x is odd apply:  3x + 1
+#If x is even apply: x/2
+#Repeat and you will always eventually end up with '1'
+
+$x = 527
+
+#Premade variables
+$Odds = @(1,3,5,7,9)
+$Evens = @(0,2,4,6,8)
+
+Clear-Host
+
+function Loop {
+    $i = 1
+    do {
+        $LastNum = $x.ToString()[$x.ToString().Length - 1]
+        $LastNum = $LastNum.ToString()
+        if ($LastNum -in $Odds) {
+            $x = (3 * $x) + 1
+        } elseif ($LastNum -in $Evens) {
+            $x = $x/2
+        }
+        Write-Host Step $i': '$x  
+        $i++
+    } until ($x -eq 1)
+}
+
+Loop

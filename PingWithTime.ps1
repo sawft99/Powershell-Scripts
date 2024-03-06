268a61e1c7a381b2d691e75ad9df127d82587a0e
sawft99 <81699231+sawft99@users.noreply.github.com>
2024-03-06 09:04:41 -0600

    Update PingWithTime.ps1
    - Output to console and file
    - Prompt if file already exists
    - Added status to output
    - Logic improvements

--- PingWithTime.ps1
+++ PingWithTime.ps1
@@ -1,15 +1,49 @@
+#Ping with time stamps
 #Made for PS7!
+
+$OutFolder = $Env:USERPROFILE + '\' + 'Downloads'
+$PingCount = '99999999'
+
+#--------
+
+Clear-Host
+
+Write-Host '
+==========================
+Ping test with time stamps
+==========================
+'
+
 function Address {
-    $global:Address = Read-Host -Prompt "Enter ip/domain to test"
-    if ($global:Address.Length -EQ 0) {
+    $Address = Read-Host -Prompt "Enter ip/domain to test"
+    if ($Address.Length -EQ 0) {
         Address
     }
-}
-Address
-function FormattedTime {
-    $Var = Get-Date
-    $TimeFinal = $Var.ToLongTimeString()
-    $TimeFinal
+    $Address
 }
 
-Test-Connection -Ping $global:Address -Count 20 | Format-Table @{Name="Time";Expression={FormattedTime}},Ping,Address,Latency
+function FormattedTime {
+    $Time = Get-Date
+    $Time = $Time.ToLongTimeString()
+    $Time
+}
+
+$Address = Address
+$OutFile = $OutFolder + '\' + $Address + '.txt'
+$TestFile = Test-Path $OutFile
+if ($TestFile -eq $true) {
+    Write-Host ''
+    $LASTEXITCODE = 0
+    choice /m 'File already exists. Delete first?'
+    Write-Host
+    if (($LASTEXITCODE -ne 1) -and ($LASTEXITCODE -ne 2)) {
+        Write-Host -ForegroundColor Red 'Error in selection
+        '
+        Pause
+        Exit 1
+    } elseif ($LASTEXITCODE -eq 1) {
+        Remove-Item $OutFile -Force
+    }
+}
+
+Test-Connection -Ping $Address -Count $PingCount -DontFragment -OutVariable Table | Select-Object * | Format-Table @{Name="Time";Expression={FormattedTime}},Ping,Address,Latency,Status -Wrap | Tee-Object -FilePath $OutFile -Append

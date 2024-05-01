1855700e4c54dadea2ae42e37347c3b63db6013d
sawft99 <81699231+sawft99@users.noreply.github.com>
2024-05-01 13:45:40 -0500

    Update GetPartialWindowsKey.ps1
    - Add log option
    - Spelling and formatting changes

--- GetPartialWindowsKey.ps1
+++ GetPartialWindowsKey.ps1
@@ -1,6 +1,32 @@
-#Gets the last few digits of a windows key. Writes to file.
+#Gets the last few characters of a windows key. Option to write to file.
 
-$OutLocation = #Location
+$LogOption = $false
+$OutLocation = 'C:\Log'
+$OutFile = $OutLocation + '\WinKeyPart.txt'
 
-$key = cscript C:\windows\system32\slmgr.vbs /dlv | findstr.exe /s "Partial"
-$key.Trim("Partial Product Key:") | Out-File -FilePath $OutLocation\winkeypart.txt
+#----------------
+
+Clear-Host
+
+Write-Host '
+-------------------
+Partial Windows Key
+-------------------
+'
+
+if (($LogOption -ne $true) -and ($LogOption -ne $false)) {
+    Write-Host -ForegroundColor Red 'Select either $true or $false for the $LogOption value
+    '
+    exit 1
+} elseif ((Test-Path $OutLocation) -ne $true) {
+    Write-Host -ForegroundColor Red 'OutLocation folder does not exist
+    '
+    exit 1
+} else {
+    $Key = cscript C:\windows\system32\slmgr.vbs /dlv | findstr.exe /s "Partial"
+    if ($LogOption -eq $true) {
+        $Key | Out-File -FilePath "$OutFile" -Force
+    }
+    Write-Host $Key
+    exit 0
+}

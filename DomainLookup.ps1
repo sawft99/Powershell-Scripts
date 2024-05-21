14f5a39df208c24065f81dbe4d8e310e9246258c
sawft99 <81699231+sawft99@users.noreply.github.com>
2024-05-21 09:58:19 -0500

    Update DomainLookup.ps1
    - Can now do multiple domains
    - It will automatically create a folder for each domain in the array at the root of the $OutputFolder
    - Formatting changes

--- DomainLookup.ps1
+++ DomainLookup.ps1
@@ -1,63 +1,79 @@
 #Lookup every possible TLD of a domain
-$DomainRoot = 'twitter'
+$DomainRoot = @('twitter','google','facebook','microsoft')
 $DNSServer = @('8.8.8.8')
 [System.IO.DirectoryInfo]$OutputFolder = 'C:\Output'
 
-#Test for parent path
-$FolderTest = Test-Path $OutputFolder
-if ($FolderTest -eq $false) {
-    Write-Host -ForegroundColor Red "
-Folder does not exist
-"
-exit 1
-}
-
 #Premade variables
+$DNSTypes = @('A_AAAA','NS','CNAME','SOA','MX','TXT','DS','RRSIG','NSEC','DNSKEY')
 $TLDSource = "https://data.iana.org/TLD/tlds-alpha-by-domain.txt"
 $AllTLD = (Invoke-WebRequest -UseBasicParsing -Uri $TLDSource).content -split "\n"
-$DNSTypes = @('A_AAAA','NS','CNAME','SOA','MX','TXT','DS','RRSIG','NSEC','DNSKEY')
 #Text document has a header so index starts at 1 and there are extra spaces at the end thus the '- 2'
 $AllTLD = $AllTLD[1..($AllTLD.Count - 2)]
-$Resolved = @()
-$ResolvedNOIP = @()
-$Collision = @()
-$Unresolved = @()
 
-#DNSLookup
-#Green if found, yellow if no ip, collision, or not yet official, red if nothing found
+#---------------
+
 Clear-Host
 
-foreach ($TLD in $AllTLD) {
-    $FullDomain = ($DomainRoot + "." + $TLD)
-    #Write-Host "Looked up " -NoNewline
-    $Lookup = foreach ($Type in $DNSTypes) {
-        $Result = Resolve-DnsName $FullDomain -Server $DNSServer -DnsOnly -Type $Type -ErrorAction SilentlyContinue
-        if (($Type -ne "SOA" ) -and ($Result.Type -eq "SOA")) {$Result = $null}
-        if (( $null -ne $Result) -and ($Result.count -gt 0)) {$Result | Export-Csv -Path "$OutputFolder\$Type.csv" -Append -NoClobber -NoTypeInformation -Force}
-        $Result
-    }
-    $Lookup = $Lookup | Select-Object -Unique
-    Write-Host "Looked up " -NoNewline
-    if (($Lookup.IPAddress.count -gt 0) -or ($Lookup.IP4Address -gt 0) -or ($Lookup.IP6Address -gt 0)) {
-        if (($Lookup.IPAddress -eq "127.0.53.53") -or ($Lookup.IP4Address -eq "127.0.53.53")) {
-            Write-Host -ForegroundColor Yellow "$FullDomain - Collision/Unofficial"
-            $Collision += $FullDomain
-        } else {
-            Write-Host -ForegroundColor Green "$FullDomain"
-            $Resolved += $FullDomain
-        }
-    } else {
-        if ($Lookup.count -gt 0) {
-            Write-Host -ForegroundColor Yellow "$FullDomain - No IP"
-            $ResolvedNOIP += $FullDomain
-        } else {
-            Write-Host -ForegroundColor Red "$FullDomain"
-            $Unresolved += $FullDomain
-        }
-    }
+Write-Host '
+=================
+Domain TLD Lookup
+=================
+'
+
+#Test for parent path
+if ($OutputFolder.Exists -eq $false) {
+    Write-Host -ForegroundColor Red 'Output folder does not exist
+'
+    exit 1
 }
 
-Clear-Host
+#DNSLookup
+#Green if found, yellow if no ip, collision, or not yet official, and red if nothing found
+
+foreach ($Domain in $DomainRoot) { 
+    [System.IO.DirectoryInfo]$DomainFolder = $OutputFolder.FullName + '\' + $Domain
+    if ($DomainFolder.Exists -eq $false) {
+        New-Item -Path $OutputFolder -Name $Domain.ToUpper() -ItemType Directory | Out-Null
+    }
+    $Resolved = @()
+    $ResolvedNOIP = @()
+    $Collision = @()
+    $Unresolved = @()
+    foreach ($TLD in $AllTLD) {
+        $FullDomain = ($Domain + "." + $TLD)
+        $Lookup = foreach ($Type in $DNSTypes) {
+            $Result = Resolve-DnsName $FullDomain -Server $DNSServer -DnsOnly -Type $Type -ErrorAction SilentlyContinue
+            if (($Type -ne "SOA" ) -and ($Result.Type -eq "SOA")) {$Result = $null}
+            if (($null -ne $Result) -and ($Result.count -gt 0)) {$Result | Export-Csv -Path "$DomainFolder\$Type.csv" -Append -NoClobber -NoTypeInformation -Force}
+            $Result
+        }
+        $Lookup = $Lookup | Select-Object -Unique
+        Write-Host "Looked up " -NoNewline
+        if (($Lookup.IPAddress.count -gt 0) -or ($Lookup.IP4Address -gt 0) -or ($Lookup.IP6Address -gt 0)) {
+            if (($Lookup.IPAddress -eq "127.0.53.53") -or ($Lookup.IP4Address -eq "127.0.53.53")) {
+                Write-Host -ForegroundColor Yellow "$FullDomain - Collision/Unofficial"
+                $Collision = $Collision + $FullDomain
+            } else {
+                Write-Host -ForegroundColor Green "$FullDomain"
+                $Resolved = $Resolved + $FullDomain
+            }
+        } else {
+            if ($Lookup.count -gt 0) {
+                Write-Host -ForegroundColor Yellow "$FullDomain - No IP"
+                $ResolvedNOIP = $ResolvedNOIP + $FullDomain
+            } else {
+                Write-Host -ForegroundColor Red "$FullDomain"
+                $Unresolved = $Unresolved + $FullDomain
+            }
+        }
+    }
+    $Resolved     | Out-File -FilePath "$DomainFolder\!Resolved.txt" -Force -Append
+    $ResolvedNOIP | Out-File -FilePath "$DomainFolder\!ResolvedNOIP.txt" -Force -Append
+    $Collision    | Out-File -FilePath "$DomainFolder\!Collision.txt" -Force -Append
+    $Unresolved   | Out-File -FilePath "$DomainFolder\!Unresolved.txt" -Force -Append
+}
+
+#Report
 Write-Host ("Resolved Domains (" + ($Resolved.count) + "):")
 Write-Host ""
 $Resolved

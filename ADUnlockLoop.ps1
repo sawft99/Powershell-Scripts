facf882912da618f4e1ec84a2c8d4d9b665dfdf3
sawft99 <81699231+sawft99@users.noreply.github.com>
2025-02-19 13:20:18 -0600

    Update ADUnlockLoop.ps1
    - Support multiple users
    - Each one gets their own log file
    - Go by Sam name instead of UPN
    - Wording change

--- ADUnlockLoop.ps1
+++ ADUnlockLoop.ps1
@@ -1,12 +1,11 @@
 #Loop on unlocking a specific user account
 
-$User = 'user@example.com' # UPN formatting
+$Users = ('User1','User2','User3') # SamAccountName
 [int]$SeriesDelay = 5 #Delay in seconds, time between each series check of account status
 [int]$ServerDelay = 0 #Delay in seconds, time between each servers check of account status
-[int]$Loops = 0 # 0 = infinite
+[int]$Loops = 0 #0 = infinite
 $LogOption = $true
-$LogFolder = 'C:\Logs'
-$LogLocation = $LogFolder + '\Unlock-' + ($User -split '@')[0] + '.txt'
+$LogFolder = "$env:USERPROFILE\Logs\Unlocks"
 
 $DomainControllers = (Get-ADGroupMember 'Domain Controllers').Name
 
@@ -20,28 +19,35 @@ Unlock User Loop
 =================
 "
 
-$User = Get-ADUser -Filter {UserPrincipalName -like $User}
-if ($null -eq $User) {
-    Write-Host -ForegroundColor Red 'User does not exist
-    '
+if (!(Test-Path $LogFolder)) {
+    Write-Host -ForegroundColor Red '$LogLocation does not exist'
     exit 1
-} else {
-    Write-Host "User: $($User.SamAccountName)
-    "
 }
 
-
+foreach ($User in $Users) {
+    $UserTest = Get-ADUser -Filter {SamAccountName -eq $User} -Properties PasswordLastSet
+    if ($null -eq $UserTest) {
+        Write-Host -ForegroundColor Red "User ($User) does not exist
+        "
+        exit 1
+    } else {
+        Write-Host "$($UserTest.SamAccountName) last password change $($UserTest.PasswordLastSet)"
+    }
+}
+Write-Host ''
+    
 function CheckLockStatus {
+    #Write-Host Checking $User
     $Status = foreach ($Server in $DomainControllers) {
         Start-Sleep $ServerDelay
         #Write-Host "Checking server $Server"
-        [string]$Time = Get-Date -Format 'MM/dd/yyyy hh:mm tt'
+        [string]$Time = Get-Date -Format 'MM/dd/yyyy hh:mm:ss tt'
         $Check = (Get-ADUser $User -Properties LockedOut -Server $Server).LockedOut
         if ($Check -eq $true) {
             #$Check | Add-Member -MemberType NoteProperty -Name Server -Value $Server
-            Write-Host "User locked on $Server at $Time"
+            Write-Host "$User locked on $Server at $Time"
             if ($LogOption -eq $true) {
-                "User locked on $Server at $Time" | Out-File -Append $LogLocaiton
+                "$User locked on $Server at $Time" | Out-File -Append $LogLocation
             }
         }
         $WholeObject = [PSCustomObject]@{
@@ -60,21 +66,21 @@ function CheckLockStatusIndividual {
 }
 
 function UnlockAccount {
-    Write-Host 'Unlocking account...'
+    Write-Host "Unlocking $User..."
     $LockedServers = ($Status | Where-Object -Property Locked -eq $true).Server
     foreach ($Server in $LockedServers) {
-        [string]$Time = Get-Date -Format 'MM/dd/yyyy hh:mm tt'
+        [string]$Time = Get-Date -Format 'MM/dd/yyyy hh:mm:ss tt'
         Unlock-ADAccount $User -Server $Server
         $Status = CheckLockStatusIndividual $Server
         if ($Status -eq $false) {
-            Write-Host "Unlocked account on server $Server at $Time"
+            Write-Host "$User unlocked on $Server at $Time"
             if ($LogOption -eq $true) {
-                "Unlocked account on server $Server at $Time" | Out-File -Append $LogLocaiton
+                "$User unlocked on $Server at $Time" | Out-File -Append $LogLocation
             }
         } else {
-            Write-Host "Failed to unlock account on server $Server at $Time"
+            Write-Host "$User failed to unlock on $Server at $Time"
             if ($LogOption -eq $true) {
-                "Failed to unlock account on server $Server at $Time" | Out-File -Append $LogLocaiton
+                "$User failed to unlock on $Server at $Time" | Out-File -Append $LogLocaiton
             }
         }
     }
@@ -82,31 +88,36 @@ function UnlockAccount {
 
 if ($Loops -eq 0) {
     do {
-       if ($null -ne $Status) {
-            Remove-Variable Status
-       }
-        $i = 0
-        $Status = CheckLockStatus
-        if (($Status.Locked -eq $true.count -gt 0)) {
-            $Time = Get-Date -Format 'MM/dd/yyyy hh:mm tt'
-            #Write-Host "Account was locked at $($Time)"
-            UnlockAccount
+        foreach ($User in $Users) {
+            $LogLocation = $LogFolder + '\Unlock-' + ($User -split '@')[0] + '.txt'
+            if ($null -ne $Status) {
+                Remove-Variable Status
+            }
+            $i = 0
+            $Status = CheckLockStatus
+            if (($Status.Locked -eq $true.count -gt 0)) {
+                $Time = Get-Date -Format 'MM/dd/yyyy hh:mm tt'
+                #Write-Host "Account was locked at $($Time)"
+                UnlockAccount
+            }
+            Start-Sleep $SeriesDelay
         }
-        Start-Sleep $SeriesDelay
     } until ($i -eq 1)
 } else {
     $i = 0
     do {
-        if ($null -ne $Status) {
-            Remove-Variable Status
+        foreach ($User in $Users) {
+            if ($null -ne $Status) {
+                Remove-Variable Status
+            }
+            $Status = CheckLockStatus
+            if (($Status.Locked -eq $true.count -gt 0)) {
+                $Time = Get-Date -Format 'MM/dd/yyyy hh:mm tt'
+                #Write-Host "Account was locked at $($Time)"
+                UnlockAccount
+            }
+            $i = $i + 1
+            Start-Sleep $SeriesDelay
         }
-        $Status = CheckLockStatus
-        if (($Status.Locked -eq $true.count -gt 0)) {
-            $Time = Get-Date -Format 'MM/dd/yyyy hh:mm tt'
-            #Write-Host "Account was locked at $($Time)"
-            UnlockAccount
-        }
-        $i = $i + 1
-        Start-Sleep $SeriesDelay
     } until ($i -eq $Loops)
 }

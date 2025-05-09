b97134203a26e2a6a7cf9643fa04fb2ec083dfc8
sawft99 <81699231+sawft99@users.noreply.github.com>
2025-05-08 20:50:48 -0500

    Update WG-VPNCheck.ps1

--- WG-VPNCheck.ps1
+++ WG-VPNCheck.ps1
@@ -6,7 +6,7 @@
 [System.IO.DirectoryInfo]$ConfigDir = $WireguardDir.FullName + 'Data\ConfigFiles\'
 #Locaiton of specific config file for this VPN check
 [System.IO.FileInfo]$ConfigFile = $ConfigDir.FullName + 'VPN.conf'
-#Whether to check if IP Wireguard is connecting to is the same as what DNS resolves to
+#Whether to check if the IP Wireguard is connecting to is the same as what DNS resolves to
 $DNSCheck = $true #or '$false'
 #DNS name Wireguard is trying to connect to, will not use DNS cache on client
 $VPNDNSName = Resolve-DnsName -DnsOnly -NoHostsFile -Type A -Name 'DOMAIN_NAME.myddns.me'
@@ -19,6 +19,10 @@ Write-Host '================
 VPN Status Check
 ================'
 
+if (($DNSCheck -ne $true) -and ($DNSCheck -ne $false)) {
+    Write-Host '$DNSCheck needs to be $true or $false'
+    exit 1
+}
 if (($WireguardDir.Exists -ne $true) -or ($ConfigDir.Exists -ne $true) -or ($ConfigFile.Exists -ne $true)) {
     Write-Host "
     Missing file or folder
@@ -43,30 +47,33 @@ if (($WireguardDir.Exists -ne $true) -or ($ConfigDir.Exists -ne $true) -or ($Con
             exit 1
         } else {
             Write-Host 'VPN back up'
-            if ($DNSCheck -eq $false) {
+            if ($DNSCheck -ne $true) {
               exit 0
             }
         }
     } else {
         Write-Host 'VPN running, exiting'
-        if ($DNSCheck -eq $false) {
+        if ($DNSCheck -ne $true) {
           exit 0
         }
     }
 }
 
 #DNS Check
-
-$VPNIP = (($VPNInfo | Select-String 'endpoint') -split ': ' -split ':')[1]
-
-if ($VPNIP -ne $VPNDNSName.IPAddress) {
-    Write-Host 'DNS and VPNIP mismatch'
-    $WireguardProcs = Get-Process 'wireguard'
-    foreach ($Proc in $WireguardProcs) {
-        Write-Host "Stopping $($Proc.ProcessName) ($($Proc.Id))"
-        Stop-Process -Id $Proc.Id -Force
+if ($DNSCheck -eq $true) {
+    $VPNIP = (($VPNInfo | Select-String 'endpoint') -split ': ' -split ':')[1]
+    if ($VPNIP -ne $VPNDNSName.IPAddress) {
+        Write-Host 'DNS and VPN IP mismatch'
+        $WireguardProcs = Get-Process 'wireguard'
+        foreach ($Proc in $WireguardProcs) {
+            Write-Host "Stopping $($Proc.ProcessName) ($($Proc.Id))"
+            Stop-Process -Id $Proc.Id -Force
+        }
+        Write-Host 'Starting VPN again...'
+        Start-Sleep -Seconds 5
+        wireguard.exe /installtunnelservice $ConfigFile
     }
-    Write-Host 'Starting VPN again...'
-    Start-Sleep -Seconds 5
-    wireguard.exe /installtunnelservice $ConfigFile
+} else {
+    Write-Host '$DNSCheck not $true, skipping'
+    exit 0
 }
